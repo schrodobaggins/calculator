f7ed6d56cb83471e757798fb47a5210a3cb30f40
Michael Schroeder <magicalmikely@gmail.com>
2022-04-01 11:56:42 -0700

    starting from scratch

--- app.js
+++ app.js
@@ -2,28 +2,3 @@
 // Created using TOP 
 // Michael Schroeder
 
-
-// instance variables
-let totalResult = 0;
-
-// integers declared here
-const one = document.querySelector("#one");
-
-// operations declared here
-const add = document.querySelector("#add");
-const subtract = document.querySelector("#subtract");
-const multiply = document.querySelector("#multiply");
-const divide = document.querySelector("#divide");
-const reset = document.querySelector("#reset");
-const result = document.querySelector("#active-results");
-
-// counter textContent set here
-result.textContent = 0;
-
-
-// userInput eventListeners here
-
-
-
-// operation functions here
-// add, subtract, multiply, divide, equals, and reset
