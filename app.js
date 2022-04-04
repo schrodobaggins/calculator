f3877089ef6205fa0eb60dcb9ea8fdcd19e81d53
Michael Schroeder <magicalmikely@gmail.com>
2022-04-04 11:47:35 -0700

    simple operations completed

--- app.js
+++ app.js
@@ -16,19 +16,20 @@ function updateDisplay() {
     display.value = calculator.displayValue;
   }
   
-  updateDisplay();
-    const keys = document.querySelector('.calculator-keys');
-    keys.addEventListener('click', (event) => {
-        // Access the clicked element
-        const { target } = event;
-        // Check if the clicked element is a button.
-        // If not, exit from the function
-        if (!target.matches('button')) {
-            return;
-        }
+updateDisplay();
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', (event) => {
+      // Access the clicked element
+      const { target } = event;
+      // Check if the clicked element is a button.
+      // If not, exit from the function
+      if (!target.matches('button')) {
+          return;
+      }
 
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
 
@@ -48,16 +49,61 @@ function updateDisplay() {
 });
 
 function inputDigit(digit) {
-    const { displayValue } = calculator;
-    // Overwrite `displayValue` if the current value is '0' otherwise append to it
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
     calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 
-  function inputDecimal(dot) {
-    // If the `displayValue` property does not contain a decimal point
-    if (!calculator.displayValue.includes(dot)) {
-      // Append the decimal point
-      calculator.displayValue += dot;
-      // extra decimal is unable to be added if already exists in DisplayValue
-    }
+  console.log(calculator);
+}
+
+function inputDecimal(dot) {
+  // If the `displayValue` property does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+    // extra decimal is unable to be added if already exists in DisplayValue
   }
+}
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+  
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+  
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === '*') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
